8b8c751c4d24824deec1b580efd36e89348dfb7a
Maarten86M <maartenmoelands@gmail.com>
2021-04-19 10:51:45 +0200

    Hele simpele test geschreven in index.test.js

---
+++ __tests___/index.test.js
@@ -0,0 +1,14 @@
+function testFunction (num1,num2){
+    return num1 + num2;
+}
+
+test("return a value when two nummers make this sum", function () {
+    // Arange
+    const number1= 5;
+    const number2 = 10;
+    //Act
+    const sum = testFunction(number1, number2);
+    //Assert
+    // geef terug wat het zou moeten zijn. Dus het antwoord in sum.
+    expect(sum).toBe(15);
+})
